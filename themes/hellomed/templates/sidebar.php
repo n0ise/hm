ee4c5208c28e9ef5e77d14a356704f75bb10d52a
Simone <tambasco@weventure.de>
2022-11-14 15:14:05 +0100

    class active dynamic based on page
    need to be completed, it will check if the page is with its name, and make the li class active

--- themes/hellomed/templates/sidebar.php
+++ themes/hellomed/templates/sidebar.php
@@ -1,8 +1,9 @@
 <div class="sidebar">
     <ul>
-      <li><a href="#">Medikationsplan</a></li>
-      <li><a href="#">Rezepte</a></li>
-      <li><a href="#">FAQ &amp; Hilfe</a></li>
-      <li><a href="/profile" class="active">Einstellungen</a></li>
+        <!-- check the page slug and make the correspondent <li> with class active -->
+        <li class="<?php if (is_page('Medikationsplan')) { echo 'active'; } ?>"><a href="/medikationsplan">Medikationsplan</a></li>
+        <li class="<?php if (is_page('rezepte')) { echo 'active'; } ?>"><a href="/rezepte">Rezepte</a></li>
+        <li class="<?php if (is_page('faq')) { echo 'active'; } ?>"><a href="/faq">FAQ &amp; Hilfe</a></li>
+        <li class="<?php if (is_page('profile')) { echo 'active'; } ?>"><a href="/profile">Einstellungen</a></li>
     </ul>
   </div>
