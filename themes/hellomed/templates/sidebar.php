ca7d0f29e89f995689ea84351622d6330fc56ba1
Simone <tambasco@weventure.de>
2022-11-16 12:47:24 +0100

    changing inn url page /hilfe

--- themes/hellomed/templates/sidebar.php
+++ themes/hellomed/templates/sidebar.php
@@ -3,7 +3,7 @@
         <!-- check the page slug and make the correspondent <li> with class active -->
         <li class="<?php if (is_page('Medikationsplan')) { echo 'active'; } ?>"><a href="/medikationsplan">Medikationsplan</a></li>
         <li class="<?php if (is_page('rezepte')) { echo 'active'; } ?>"><a href="/rezepte">Rezepte</a></li>
-        <li class="<?php if (is_page('faq')) { echo 'active'; } ?>"><a href="/faq">FAQ &amp; Hilfe</a></li>
+        <li class="<?php if (is_page('hilfe')) { echo 'active'; } ?>"><a href="/hilfe">FAQ &amp; Hilfe</a></li>
         <li class="<?php if (is_page('profile')) { echo 'active'; } ?>"><a href="/profile">Einstellungen</a></li>
     </ul>
   </div>
