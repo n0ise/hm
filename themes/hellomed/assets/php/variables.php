bcef14e78014d7028bcf136719f1dc98ee61a0b1
Simone <tambasco@weventure.de>
2022-11-18 12:30:59 +0100

    Update variables.php
    added new acf fields (from asana) as variables and adjusted the $days_left

--- themes/hellomed/assets/php/variables.php
+++ themes/hellomed/assets/php/variables.php
@@ -27,25 +27,28 @@ $allerg = get_field('allergies', 'user_' . $user_id);
 $user_city = get_field('stadt', 'user_' . $user_id);
 $rezept = get_field('rezept', 'user_' . $user_id);
 $user_zip = get_field('postcode', 'user_' . $user_id);
-$user_country = get_field('strasse', 'user_' . $user_id);
-$status = get_field('status', 'user_' . $user_id);
+$user_strasse = get_field('strasse', 'user_' . $user_id);
+$insurance_company = get_field('insurance_company', 'user_' . $user_id);
+$insurance_number = get_field('insurance_number', 'user_' . $user_id);
+$sickness = get_field('sickness', 'user_' . $user_id); 
+$gender = get_field('geschlecht', 'user_' . $user_id);
+$medikamente= get_field('medikamente', 'user_' . $user_id);
+$start_date = get_field('start_date', 'user_' . $user_id);
 $status = get_field('status', 'user_' . $user_id);
 // if status is disabled, make it red. this is a visual silly test
  if ($status == 'Aktive') {
      $status = '<span style="color:green;">' . $status . '</span>';
  }
 else {  $status = '<span style="color:red;">' . $status . '</span>'; }
-// show the rezept_end with ymd
-
 $rezept_end = get_field('rezept_end', 'user_' . $user_id);
 // show how many days left in human readable format
  $days_left = human_time_diff( strtotime($rezept_end), current_time('timestamp') );
 
 // if days left are less than 30, make it red. this is another visual silly test
-  if ($days_left < 7) {
-      $days_left = '<span style="color:red;">Expiring in ' . $days_left . '</span>';
+  if ($days_left > 14) {
+      $days_left = '<span style="color:red;">Expired ' . $days_left . '</span>';
   }
- else {  $days_left = '<span style="color:green;">Expired ' . $days_left . '</span>'; }
+ else {  $days_left = '<span style="color:green;">Expiring in ' . $days_left . '</span>'; }
 
 // it is same for $user_id, this is for testing, it is inside acf, may be deleted later (or used for matching with AVOCADO API Request)
  $new_user_id = get_field('new_user_id');
