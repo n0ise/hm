9543fc3508ec17386c60c0b4e4c1113f04d9bb27
Simone <tambasco@weventure.de>
2022-12-02 18:17:02 +0100

    Update os-medications.js

--- themes/hellomed/assets/js/os-medications.js
+++ themes/hellomed/assets/js/os-medications.js
@@ -3,54 +3,76 @@ const response = {
     {
       "medication": "Aspirin",
       "dosages": [
-        { "date": "2022-10-31", "time": "09:00" },
-        { "date": "2022-11-02", "time": "09:00" },
-        { "date": "2022-11-04", "time": "09:00" },
-        { "date": "2022-11-06", "time": "09:00" }
+        { "date": "2022-10-31", "time": "09:00", "amount": "1" },
+        { "date": "2022-11-02", "time": "09:00", "amount": "1" },
+        { "date": "2022-11-04", "time": "09:00", "amount": "1" },
+        { "date": "2022-11-06", "time": "09:00", "amount": "1" }
       ]
     },
     {
       "medication": "Paracetamol",
       "dosages": [
-        { "date": "2022-10-31", "time": "13:00" },
-        { "date": "2022-11-01", "time": "15:00" },
-        { "date": "2022-11-03", "time": "15:00" },
-        { "date": "2022-11-04", "time": "13:00" },
-        { "date": "2022-11-06", "time": "13:00" }
+        { "date": "2022-10-31", "time": "13:00", "amount": "2" },
+        { "date": "2022-11-01", "time": "09:00", "amount": "1" },
+        { "date": "2022-11-03", "time": "19:00", "amount": "1" },
+        { "date": "2022-11-04", "time": "13:00", "amount": "2" },
+        { "date": "2022-11-06", "time": "13:00", "amount": "1" }
       ]
     },
     {
       "medication": "Ibuprofen",
       "dosages": [
-        { "date": "2022-10-31", "time": "19:00" },
-        { "date": "2022-11-01", "time": "20:00" },
-        { "date": "2022-11-02", "time": "19:00" },
-        { "date": "2022-11-03", "time": "20:00" },
-        { "date": "2022-11-04", "time": "19:00" },
-        { "date": "2022-11-05", "time": "20:00" },
-        { "date": "2022-11-06", "time": "19:00" }
+        { "date": "2022-10-31", "time": "19:00", "amount": "2" },
+        { "date": "2022-11-02", "time": "19:00", "amount": "1" },
+        { "date": "2022-11-04", "time": "19:00", "amount": "1" },
+        { "date": "2022-11-06", "time": "09:00", "amount": "1" }
       ]
     },
     {
       "medication": "Vicodin",
       "dosages": [
-        { "date": "2022-10-31", "time": "09:00" },
-        { "date": "2022-10-31", "time": "13:00" },
-        { "date": "2022-10-31", "time": "19:00" },
-        { "date": "2022-11-01", "time": "13:00" },
-        { "date": "2022-11-01", "time": "19:00" },
-        { "date": "2022-11-02", "time": "13:00" },
-        { "date": "2022-11-02", "time": "19:00" },
-        { "date": "2022-11-03", "time": "09:00" },
-        { "date": "2022-11-03", "time": "13:00" },
-        { "date": "2022-11-04", "time": "09:00" },
-        { "date": "2022-11-04", "time": "13:00" },
-        { "date": "2022-11-04", "time": "19:00" },
-        { "date": "2022-11-05", "time": "13:00" },
-        { "date": "2022-11-05", "time": "19:00" },
-        { "date": "2022-11-06", "time": "09:00" },
-        { "date": "2022-11-06", "time": "13:00" },
-        { "date": "2022-11-06", "time": "19:00" }
+        { "date": "2022-10-31", "time": "09:00", "amount": "2" },
+        { "date": "2022-10-31", "time": "13:00", "amount": "1" },
+        { "date": "2022-10-31", "time": "19:00", "amount": "1" },
+        { "date": "2022-11-01", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-01", "time": "19:00", "amount": "2" },
+        { "date": "2022-11-02", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-02", "time": "19:00", "amount": "1" },
+        { "date": "2022-11-03", "time": "09:00", "amount": "1" },
+        { "date": "2022-11-03", "time": "13:00", "amount": "2" },
+        { "date": "2022-11-04", "time": "09:00", "amount": "1" },
+        { "date": "2022-11-04", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-04", "time": "19:00", "amount": "1" },
+        { "date": "2022-11-05", "time": "13:00", "amount": "2" },
+        { "date": "2022-11-05", "time": "19:00", "amount": "1" },
+        { "date": "2022-11-06", "time": "09:00", "amount": "1" },
+        { "date": "2022-11-06", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-06", "time": "19:00", "amount": "1" }
+      ]
+    },
+    {
+      "medication": "Dolormin",
+      "dosages": [
+        { "date": "2022-10-31", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-01", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-02", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-03", "time": "13:00", "amount": "2" },
+        { "date": "2022-11-04", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-05", "time": "13:00", "amount": "2" },
+        { "date": "2022-11-06", "time": "13:00", "amount": "1" }
+      ]
+    }
+    ,
+    {
+      "medication": "Oxycodon",
+      "dosages": [
+        { "date": "2022-10-31", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-01", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-02", "time": "13:00", "amount": "2" },
+        { "date": "2022-11-03", "time": "13:00", "amount": "1" },
+        { "date": "2022-11-04", "time": "13:00", "amount": "2" },
+        { "date": "2022-11-05", "time": "09:00", "amount": "1" },
+        { "date": "2022-11-06", "time": "13:00", "amount": "1" }
       ]
     }
   ]
@@ -68,19 +90,19 @@ const response = {
 *     {
 *       "medication": "Aspirin",
 *       "dosages": [
-*         { "date": "2022-10-31", "time": "09:00" },
-*         { "date": "2022-10-17", "time": "19:00" },
-*         { "date": "2022-11-01", "time": "09:00" },
-*         { "date": "2022-11-01", "time": "19:00" }
+*         { "date": "2022-10-31", "time": "09:00", amount: 1 },
+*         { "date": "2022-10-17", "time": "19:00", amount: 1 },
+*         { "date": "2022-11-01", "time": "09:00", amount: 1 },
+*         { "date": "2022-11-01", "time": "19:00", amount: 1 }
 *       ]
 *     },
 *     {
 *       "medication": "Paracetamol",
 *       "dosages": [
-*         { "date": "2022-10-31", "time": "09:00" },
-*         { "date": "2022-10-31", "time": "19:00" },
-*         { "date": "2022-11-01", "time": "09:00" },
-*         { "date": "2022-11-01", "time": "19:00" }
+*         { "date": "2022-10-31", "time": "09:00", amount: 1 },
+*         { "date": "2022-10-31", "time": "19:00", amount: 1 },
+*         { "date": "2022-11-01", "time": "09:00", amount: 1 },
+*         { "date": "2022-11-01", "time": "19:00", amount: 1 }
 *       ]
 *     }
 *   ]
@@ -98,15 +120,15 @@ const response = {
 *   {
 *     day: '2022-10-31',
 *     med: [
-*       { time: '9:00', names: ['Aspirin', 'Paracetamol'] },
-*       { time: '19:00', names: ['Aspirin', 'Paracetamol'] }
+*       { time: '9:00', names: [{ name: 'Aspirin', amount: 1 }, { name: 'Paracetamol', amount: 1 }] },
+*       { time: '19:00', names: [{ name: 'Aspirin', amount: 1 }, { name: 'Paracetamol', amount: 1 }] }
 *     ]
 *   },
 *   {
 *     day: '2022-11-01',
 *     med: [
-*       { time: '9:00', names: ['Aspirin', 'Paracetamol'] },
-*       { time: '19:00', names: ['Aspirin', 'Paracetamol'] }
+*       { time: '9:00', names: [{ name: 'Aspirin', amount: 1 }, { name: 'Paracetamol', amount: 1 }] },
+*       { time: '19:00', names: [{ name: 'Aspirin', amount: 1 }, { name: 'Paracetamol', amount: 1 }] }
 *     ]
 *   }
 * ]
@@ -129,7 +151,7 @@ const rewriteResponse = (object) => {
       newDate: dosage.date,
       newMeds: [{
         newTime: dosage.time,
-        newName: [med.medication]
+        newName: [ { name: med.medication, amount: dosage.amount } ]
       }]
     }))
   })
@@ -173,24 +195,51 @@ console.log(desiredResponse)
 */
 
 desiredResponse.forEach((entry) => {
-  document.querySelector('.medplan-tabs').insertAdjacentHTML('beforeend', `<div class="medplan-tab"><div class="medplan-tab-day">Mo</div><div class="medplan-tab-date">${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}</div></div>`)
+  const html = `
+    <div class="hm-medplan-tab">
+      <div class="hm-medplan-tab-day">
+        ${new Intl.DateTimeFormat('de-DE', { weekday: 'short' }).format(new Date(entry.day))}
+      </div>
+      <div class="hm-medplan-tab-date">
+        ${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}
+      </div>
+    </div>
+  `
+
+  document.querySelector('.hm-medplan-tabs').insertAdjacentHTML('beforeend', html)
 })
 
 desiredResponse.forEach((entry) => {
-  let html = `<div class="medplan-day">`
+  let html = `<div class="hm-medplan-day">`
 
-  html += `<h2>${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}</h2>`
+  html += `<h2><i class="bi bi-calendar-event"></i> ${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}</h2>`
 
-  entry.med.forEach((cur) => {
-    html += `
-      <p><b>${cur.time}</b></p>
-      <p>${cur.names.join(', ')}</p>
-    `
+  entry.med.forEach((cur, i) => {
+    if (i === 0) html += `<div class="hm-medplan-daytime"><i class="bi bi-clock"></i> ${cur.time}</div>`
+    if (i === 1) html += `<div class="hm-medplan-daytime"><i class="bi bi-clock"></i> ${cur.time}</div>`
+    if (i === 2) html += `<div class="hm-medplan-daytime"><i class="bi bi-clock"></i> ${cur.time}</div>`
+
+    html += `<div class="hm-medplan-pills">`
+
+    cur.names.forEach((cur) => {
+      html += `
+        <div class="hm-medplan-pills-pill">
+          <div>
+            <i class="bi bi-capsule-pill"></i>
+          </div>
+          <div>
+            ${cur.amount} Ã— ${cur.name}
+          </div>
+        </div>
+      `
+    })
+
+    html += `</div>`
   })
 
   html += `</div>`
 
-  document.querySelector('.content').insertAdjacentHTML('beforeend', html)
+  document.querySelector('.hm-content').insertAdjacentHTML('beforeend', html)
 })
 
 
@@ -201,8 +250,8 @@ desiredResponse.forEach((entry) => {
 * Fourth, we give the tabs some functionallity
 */
 
-const tabs = document.querySelectorAll('.medplan-tab')
-const days = document.querySelectorAll('.medplan-day')
+const tabs = document.querySelectorAll('.hm-medplan-tab')
+const days = document.querySelectorAll('.hm-medplan-day')
 
 tabs.forEach((cur, i) => {
   cur.addEventListener('click', () => {
