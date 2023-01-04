dbef38125c23f0bcdb69fd18f60c3844ddac402d
Simone <tambasco@weventure.de>
2023-01-04 16:53:31 +0100

    /os-medikationsplan API output with JS
    scraping this ee96ee4e0fc593ab1e71a4eefd3ef4721034663e and keeping it super simple.
    
    - added modal placeholder
    - showing first 7 items
    
    TBD-
    next 7 days when clicking on the chevron

--- themes/hellomed/assets/js/medication.js
+++ themes/hellomed/assets/js/medication.js
@@ -1,239 +1,205 @@
 var url = '/wp-content/themes/hellomed/assets/php/test_api.php';
-
 fetch(url, {
-    method: 'GET'
+  method: 'GET'
+})
+.then((res) => res.json())
+.then((data) => {
+  console.log(data);
+
+// sapmle output from json 
+//   [
+//     {
+//         "pzn": "766759",
+//         "medication": "Ramipril 1A Pharma Tab 5mg     1 A PHARMA GMBH               TAB",
+//         "description": "",
+//         "stock": "0",
+//         "coverage": "2022-12-08",
+//         "agent": "Ramipril",
+//         "hint": "Mit 1 Gl..Wasser vor,zum o.nach d.Früh- \n stück Cave:NSAR o.Salicylate durch Pa- \n racetamol ersetzen..Kochsalz >5g/Tag. \n Alkoholtolranz herabgesetzt. Oft Schwin- \n del, Sehstörung. Cave:Desensibilisserg! \n (Soweit nicht anders verordnet)",
+//         "image": "timages/900001232.jpg",
+//         "doctor": {
+//             "salutation": "Herr",
+//             "title": "Dr. med.",
+//             "firstname": "Matthias /Dirk",
+//             "lastname": null,
+//             "phone": "",
+//             "mobile": "",
+//             "fax": "",
+//             "email": ""
+//         },
+//         "dosages": [
+//             {
+//                 "date": "2022-12-23",
+//                 "time": "19:00",
+//                 "timeHint": "abends",
+//                 "amount": "1",
+//                 "hint": ""
+//             },
+//             {
+//                 "date": "2022-12-24",
+//                 "time": "19:00",
+//                 "timeHint": "abends",
+//                 "amount": "1",
+//                 "hint": ""
+//             },
+//             {
+//                 "date": "2022-12-25",
+//                 "time": "19:00",
+//                 "timeHint": "abends",
+//                 "amount": "1",
+//                 "hint": ""
+//             }
+//]
+
+  // Create an array to store the grouped and sorted data
+  const response = data;
+
+  // Loop through all medications and dosages
+  // for (let i = 0; i < data.medications.length; i++) {
+  //   const medication = data.medications[i];
+  //   for (let j = 0; j < medication.dosages.length; j++) {
+  //     const dosage = medication.dosages[j];
+
+  //     // Push an object containing the relevant information to the response array
+  //     response.push({
+  //       newDate: dosage.date,
+  //       newMeds: [{
+  //         newTime: dosage.time,
+  //         newName: [{
+  //           name: medication.medication,
+  //           amount: dosage.amount
+  //         }]
+  //       }]
+  //     });
+  //   }
+  // ]
+
+/*******************************************************************************
+ * First, we create a new object structure that is easier to merge
+ */
+
+const rewriteResponse = (object) => {
+  const apiArray = object.medications
+  const newArray = []
+
+  apiArray.forEach((med) => {
+    med.dosages.forEach((dosage) => newArray.push({
+      newDate: dosage.date,
+      newMeds: [{
+        newTime: dosage.time,
+        newName: [ { name: med.medication, amount: dosage.amount } ]
+      }]
+    }))
   })
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
 
+  return newArray
+}
 
-//  loop an show PZN and medication name 
+const newResponse = rewriteResponse(response)
 
-
-    // loop through the data and add it to the table
-    for (var i = 0; i < data.medications.length; i++) {
-      // and for each medication loop through the dosages
-
-      
-    // for (var i = 0; i < data.medications.dosages.length; i++) {
-
-console.log(data.medications[i].dosages[i].date);
-
-
-
-  const response = []
-
-    data.medications.forEach((medication) => {
-      medication.dosages.forEach((dosage) => response.push({
-        newDate: dosage.date,
-        newMeds: [{
-          newTime: dosage.time,
-          newName: [{
-            name: medication.medication,
-            amount: dosage.amount
-          }]
-        }]
-      }))
-      console.log('response')
-
-      console.log(response)
-
-    })
-
-
-        const rewriteResponse = (object) => {
-        const apiArray = data.medications
-        const newArray = []
-    
-          apiArray.forEach((med) => {
-            med.dosages.forEach((dosage) => newArray.push({
-              newDate: dosage.date,
-              newMeds: [{
-                newTime: dosage.time,
-                newName: [{
-                  name: med.medication,
-                  amount: dosage.amount
-                }]
-              }]
-            }))
-            console.log('new array')
-
-            console.log(newArray)
-
-          })
-    
-          // return newArray
-        }
-    
-        const newResponse = rewriteResponse(response)
-        console.log('new response')
-
-        console.log(newResponse)
-      
-        const desiredResponse = _(response).groupBy('newDate').map((items, date) => {
-          return {
-            day: date,
-            med: _(items).flatMap('newMeds').groupBy('newTime').map((items, time) => {
-              return {
-                time: time,
-                names: _.flatMap(items, 'newName')
-              }
-            }).value().sort((t1, t2) => t1.time.substring(0, 2) - t2.time.substring(0, 2))
-          }
-        }).value().sort((d1, d2) => new Date(d1.day) - new Date(d2.day))
-        console.log('desiredResponse;');
-
-        console.log(desiredResponse);
-    
-    
-    
-    
-    
-        /*******************************************************************************
-         * Third, we render the new object to the DOM, but just the first 3 days and the rest when clicking chevron right or left
-         * We also add the active class to the first tab
-         * 
-         * 
-         */
-
-        // render the first 3 days
-        desiredResponse.slice(0, 7).forEach((entry) => {
-          const html = `
-        <div class="hm-medplan-tab">
-          
-          <div class="hm-medplan-tab-day">
-            ${new Intl.DateTimeFormat('de-DE', { weekday: 'short' }).format(new Date(entry.day))}
-          </div>
-          <div class="hm-medplan-tab-date">
-            ${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}
-          </div>
-        </div>
-      `
-
-          document.querySelector('.hm-medplan-tabs').insertAdjacentHTML('beforeend', html)
-        })
-
-        // add the active class to the first tab and when clicking on a new tab, remove the active class from the old tab and add it to the new tab
-        document.querySelector('.hm-medplan-tab').classList.add('active')
-
-
-        // render the rest of the days when clicking  class "bi-chevron-right"  
-        document.querySelector('.hm-medplan-tabs-right').addEventListener('click', () => {
-// make first tab active when clicking chevron right and remove active class from the last tab 
-         
-         
-
-
-          document.querySelector('.hm-medplan-tabs').innerHTML = ''
-          desiredResponse.slice(7, 14).forEach((entry) => {
-            const html = `
-            
-          <div class="hm-medplan-tab">
-
-            <div class="hm-medplan-tab-day">
-              ${new Intl.DateTimeFormat('de-DE', { weekday: 'short' }).format(new Date(entry.day))}
-            </div>
-            <div class="hm-medplan-tab-date">
-              ${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}
-            </div>
-          </div>
-        `
-            document.querySelector('.hm-medplan-tabs').insertAdjacentHTML('beforeend', html)    
-            // make first tab active when clicking chevron right and remove active class from the last tab
-            document.querySelector('.hm-medplan-tab').classList.add('active')
-            // when clicking a new tab, remove the active class from the old tab and add it to the new tab
-            document.querySelectorAll('.hm-medplan-tab').forEach((tab) => {
-              tab.addEventListener('click', () => {
-                document.querySelector('.hm-medplan-tab.active').classList.remove('active')
-                tab.classList.add('active')
-              })
-            }
-            )
-
-          })
-        })
-
-      //  tab chevron left inactive at the beginning, but active and showing preivous days when clicking chevron left
-        document.querySelector('.hm-medplan-tabs-left').addEventListener('click', () => {
-          document.querySelector('.hm-medplan-tabs').innerHTML = ''
-          desiredResponse.slice(0, 7).forEach((entry) => {
-            const html = `
-          <div class="hm-medplan-tab">
-
-            <div class="hm-medplan-tab-day">
-              ${new Intl.DateTimeFormat('de-DE', { weekday: 'short' }).format(new Date(entry.day))}
-            </div>
-            <div class="hm-medplan-tab-date">
-              ${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}
-            </div>
-          </div>
-        `
-            document.querySelector('.hm-medplan-tabs').insertAdjacentHTML('beforeend', html)
-            document.querySelectorAll('.hm-medplan-tab').forEach((tab) => {
-              tab.addEventListener('click', () => {
-                document.querySelector('.hm-medplan-tab.active').classList.remove('active')
-                tab.classList.add('active')
-              })
-            }
-            )
-          })
-        })
-        
-
-        /*******************************************************************************
-         * Fourth, we render the first day of the new object to the DOM
-         * 
-         * 
-         * 
-         * 
-         * 
-         */ 
-         
-        // render the first day
-        desiredResponse[0].med.forEach((entry) => {
-          const html = `
-        <div class="hm-medplan-time">
-          <div class="hhm-medplan-daytime bi bi-clock">
-            ${entry.time}
-          </div>
-          <div class="hm-medplan-pills-pill ">
-            ${entry.names.map((name) => name.name).join(', ')}
-          </div>
-          <div class="hm-medplan-time-amounts">
-
-            ${entry.names.map((name) => name.amount).join(', ')}
-          </div>
-        </div>
-      `
-          document.querySelector('.hm-content').insertAdjacentHTML('beforeend', html)
-        })
-
-        /*******************************************************************************
-         * 
-         * Fifth, we render the rest of the days when clicking on the tabs
-         * 
-         */
-      
+console.log(newResponse)
 
 
 
 
 
-    
-    // mae the tab active when clicked 
-    document.querySelectorAll('.hm-medplan-tab').forEach((tab) => {
-      tab.addEventListener('click', () => {
-        document.querySelectorAll('.hm-medplan-tab').forEach((tab) => {
-          tab.classList.remove('active')
-        })
-        tab.classList.add('active')
-      })
-    })
+/*******************************************************************************
+* Second, we use lodash to merge all matching dates and times together
+* https://stackoverflow.com/questions/42081375/lodash-group-and-populate-arrays
+*/
 
-
+const desiredResponse = _(newResponse).groupBy('newDate').map((items, date) => {
+  return {
+    day: date,
+    med: _(items).flatMap('newMeds').groupBy('newTime').map((items, time) => {
+      return {
+        time: time,
+        names: _.flatMap(items, 'newName')
       }
-    }
-  
+    }).value().sort((t1, t2) => t1.time.substring(0, 2) - t2.time.substring(0, 2))
+  }
+}).value().sort((d1, d2) => new Date(d1.day) - new Date(d2.day))
 
-);
+console.log(desiredResponse)
+
+
+
+
+
+/*******************************************************************************
+* Third, we render the new object to the DOM
+*/
+
+desiredResponse.slice(0, 7).forEach((entry) => {
+  const html = `
+    <div class="hm-medplan-tab">
+      <div class="hm-medplan-tab-day">
+        ${new Intl.DateTimeFormat('de-DE', { weekday: 'short' }).format(new Date(entry.day))}
+      </div>
+      <div class="hm-medplan-tab-date">
+        ${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}
+      </div>
+    </div>
+  `
+
+  document.querySelector('.hm-medplan-tabs').insertAdjacentHTML('beforeend', html)
+})
+
+
+desiredResponse.forEach((entry) => {
+  let html = `<div class="hm-medplan-day">`
+
+  html += `<h2><i class="bi bi-calendar-event"></i> ${new Intl.DateTimeFormat('de-DE', { day: '2-digit', month: '2-digit' }).format(new Date(entry.day))}</h2>`
+
+  entry.med.forEach((cur, i) => {
+    if (i === 0) html += `<div class="hm-medplan-daytime"><i class="bi bi-clock"></i> ${cur.time}</div>`
+    if (i === 1) html += `<div class="hm-medplan-daytime"><i class="bi bi-clock"></i> ${cur.time}</div>`
+    if (i === 2) html += `<div class="hm-medplan-daytime"><i class="bi bi-clock"></i> ${cur.time}</div>`
+
+    html += `<div class="hm-medplan-pills">`
+
+    cur.names.forEach((cur) => {
+      html += `
+        <div class="hm-medplan-pills-pill" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <div>
+            <i class="bi bi-capsule-pill"></i>
+          </div>
+          <div>
+            ${cur.amount} × ${cur.name}
+          </div>
+        </div>
+      `
+    })
+
+    html += `</div>`
+  })
+
+  html += `</div>`
+
+  document.querySelector('.hm-content').insertAdjacentHTML('beforeend', html)
+})
+
+
+
+
+
+/*******************************************************************************
+* Fourth, we give the tabs some functionallity
+*/
+
+const tabs = document.querySelectorAll('.hm-medplan-tab')
+const days = document.querySelectorAll('.hm-medplan-day')
+
+tabs.forEach((cur, i) => {
+  cur.addEventListener('click', () => {
+    const prev = [...tabs, ...days]
+    const next = [cur, days[i]]
+
+    prev.forEach((el) => el.classList.remove('active'))
+    next.forEach((el) => el.classList.add('active'))
+  })
+})
+
+tabs[0].click()
+
+})
--- themes/hellomed/templates/os-medikationsplan.php
+++ themes/hellomed/templates/os-medikationsplan.php
@@ -10,24 +10,44 @@ Template Name: OS Medikationsplan
 $user_id = get_current_user_id();
 $rezepte_file = get_field('rezept_input', 'user_'. $user_id); ?>
 <main>
-  <div class="container">
-    <div class="hm-content">
+    <div class="container">
+        <div class="hm-content">
 
-      <div class="h2 mb-5">
-        Medikationsplan
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/onboarding/packaging.svg">
-      </div>
-      <div class="hm-medplan-tabs-left">
-          <i class="bi bi-chevron-left"></i>
-        </div><div class="hm-medplan-tabs mb-5">
-       
-        <div class="hm-medplan-tabs-right">
-          <i class="bi bi-chevron-right"></i>
+
+            <div class="h2 mb-5">
+                Medikationsplan
+                <img src="wp-content/themes/hellomed/assets/img/icons/onboarding/packaging.svg">
+            </div>
+            <div class="hm-medplan-tabs mb-5">
+                <div class="hm-medplan-tabs-left is-inactive">
+                    <i class="bi bi-chevron-left"></i>
+                </div>
+                <div class="hm-medplan-tabs-right">
+                    <i class="bi bi-chevron-right"></i>
+                </div>
+            </div>
+
         </div>
-      </div>
+
 
     </div>
-  </div>
+    <!-- modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Demo Modal</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim quas, tenetur veritatis
+                    architecto
+                    quia ea impedit voluptas nisi eos, neque minus accusamus nobis odio obcaecati voluptatum porro
+                    dolorum soluta iure.
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 <?php
  
