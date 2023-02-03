38bd9be3a5d308ed5854c70eedd549b59a62cc91
pepkolajk <klodianpepkola@hotmail.com>
2023-02-03 12:21:59 +0100

    fix upload

--- themes/hellomed/uploads/upload.php
+++ themes/hellomed/uploads/upload.php
@@ -6,14 +6,15 @@ global $current_user;
 get_currentuserinfo();
 $logged_in_user = $current_user->ID;
 
-$uploaddir = './' .$logged_in_user.'_'.date('d.m.Y H:i:s'). '/'; 
+$uploaddir = './' .$logged_in_user. '/'; 
 
 if (!file_exists($uploaddir)) {
     mkdir($uploaddir);
+    $index = fopen($uploaddir . "/index.html", "w");
+    fclose($index);
 }
 
 $my_file = $_FILES['my_file'];
 $file_path = $my_file['tmp_name']; // temporary upload path of the file
 $file_name = $_POST['name']; // desired name of the file
 move_uploaded_file($file_path, $uploaddir . basename($file_name)); 
-
