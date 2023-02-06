e6373712bce1f2a7ae5a76b50086a63e56b70cd4
pepkolajk <klodianpepkola@hotmail.com>
2023-02-06 13:12:27 +0100

    disable  confirmation prompt

--- themes/hellomed/functions.php
+++ themes/hellomed/functions.php
@@ -1047,3 +1047,17 @@ function add_new_field() {
 	$form->add_field( $new_field );
 	$form->save();
 }
+
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
+function logout_without_confirm($action, $result)
+{
+    /**
+     * Allow logout without confirmation
+     */
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) {
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : 'wp-login.php?loggedout=true';
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));
+        header("Location: $location");
+        die;
+    }
+}
