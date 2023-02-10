9d696b26f3b977e867ab45556137516ca023c597
pepkolajk <klodianpepkola@hotmail.com>
2023-02-10 16:39:36 +0100

    Revert "Login redirect"
    This reverts commit 02d5066badb81dbb824b6a4e17ba398ca1c8753a.

--- themes/hellomed/functions.php
+++ themes/hellomed/functions.php
@@ -1075,12 +1075,3 @@ function logout_without_confirm($action, $result)
         die;
     }
 }
-
-// login redirect
-function redirect_login() {
-    if ( $_SERVER['REQUEST_URI'] == '/anmelden' ) {
-        wp_redirect( '/login' );
-        exit;
-    }
-}
-add_action( 'template_redirect', 'redirect_login' );
