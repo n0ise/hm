c03934a638c777793c960bae6e7ceaacbed467d9
Simone <tambasco@weventure.de>
2023-02-10 12:28:07 +0100

    GTM in OS pages
    - adding the GTM code by checking first if the cookie is consented on Borlabs
    - code will be displayed only in OS pages
    /onboarding
    /rezepte
    /medikamente
    /einstellungen
    /registrieren
    /medikatonsplan

--- themes/hellomed/templates/footer.php
+++ themes/hellomed/templates/footer.php
@@ -1,18 +1,17 @@
-
 <footer class="hm-footer">
-  <div class="container">
-    <div>
-      <a href="/impressum" target="_blank">Impressum</a>
-      <a href="/agb" target="_blank">AGB</a>
-      <a href="/datenschutz" target="_blank">Datenschutz</a>
+    <div class="container">
+        <div>
+            <a href="/impressum" target="_blank">Impressum</a>
+            <a href="/agb" target="_blank">AGB</a>
+            <a href="/datenschutz" target="_blank">Datenschutz</a>
+        </div>
+        <div>
+            <img src="https://hellomed.com/wp-content/uploads/2022/05/Artboard.svg">
+            <img src="https://hellomed.com/wp-content/uploads/2022/05/Artboard-Copy.svg">
+            <img src="https://hellomed.com/wp-content/uploads/2022/05/Artboard-Copy-2.svg">
+            <img src="https://hellomed.com/wp-content/uploads/2022/05/Artboard-Copy-3.svg">
+        </div>
     </div>
-    <div>
-      <img src="https://hellomed.com/wp-content/uploads/2022/05/Artboard.svg">
-      <img src="https://hellomed.com/wp-content/uploads/2022/05/Artboard-Copy.svg">
-      <img src="https://hellomed.com/wp-content/uploads/2022/05/Artboard-Copy-2.svg">
-      <img src="https://hellomed.com/wp-content/uploads/2022/05/Artboard-Copy-3.svg">
-    </div>
-  </div>
 </footer>
 
 <script src="https://ui.hellomed.com/src/v1.1/js/bootstrap.5.3.0.bundle.min.js"></script>
@@ -23,8 +22,48 @@
 
 
 <script>
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-        })
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 </script>
+
+<!-- google GTM in all os pages, except /admin-* pages -->
+<?php 
+if (!preg_match('/^\/admin-/', $_SERVER['REQUEST_URI'])) {
+  require( ABSPATH . 'wp-blog-header.php' );
+  // $cookieStatus = BorlabsCookie\Cookie\Config::getInstance()->get('google-tag-manager');
+  $GTM_track_consent=BorlabsCookieHelper()->gaveConsent('google-tag-manager');
+
+  var_dump($GTM_track_consent);
+
+  // Check the status of the GTM cookie consent , it's a boolean
+  if ($GTM_track_consent === false || $GTM_track_consent === 0) {
+    
+      // echo "This content requires cookies to be set.";
+    
+      
+  } else {
+      ?>
+  <!-- show GTM code  -->
+  <script>
+  (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+          "gtm.start": new Date().getTime(),
+          event: "gtm.js"
+      });
+      var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src =
+          "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j, f);
+  })(window, document, "script", "dataLayer", "GTM-P4KJF2M");
+  </script>
+  <?php
+      // echo "cookie consented to view this content.";
+  }
+}
+?>
