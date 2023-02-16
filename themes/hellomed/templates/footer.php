cd90b82ea5caca36b2411ac96072e385fd0effd2
Simone <tambasco@weventure.de>
2023-02-16 15:52:27 +0100

    Added Facebook Pixel loads conditionally
    - if borlabs consent is true, loads Facebook Pixel.
    same as happening for GTM with commit here c03934a638c777793c960bae6e7ceaacbed467d9
    
    Ticket here https://app.asana.com/0/1203920041477288/1203969940514809/f

--- themes/hellomed/templates/footer.php
+++ themes/hellomed/templates/footer.php
@@ -36,6 +36,8 @@ if (!preg_match('/^\/admin-/', $_SERVER['REQUEST_URI'])) {
 
   // this is the borlabs attribute for the google-tag-manager ID, ids can be seen in the Cookie settings in the borlabs plugin
   $GTM_track_consent=BorlabsCookieHelper()->gaveConsent('google-tag-manager');
+  $FACEBOOK_track_consent=BorlabsCookieHelper()->gaveConsent('facebook-pixel');
+// var_dump($FACEBOOK_track_consent);
 
   // var_dump($GTM_track_consent);
 
@@ -47,25 +49,62 @@ if (!preg_match('/^\/admin-/', $_SERVER['REQUEST_URI'])) {
       
   } else {
       ?>
-  <!-- show GTM code  -->
-  <script>
-  (function(w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-          "gtm.start": new Date().getTime(),
-          event: "gtm.js"
-      });
-      var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src =
-          "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-  })(window, document, "script", "dataLayer", "GTM-P4KJF2M");
-  </script>
-  <?php
+<!-- show GTM code  -->
+<script>
+(function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+        "gtm.start": new Date().getTime(),
+        event: "gtm.js"
+    });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+    j.async = true;
+    j.src =
+        "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "GTM-P4KJF2M");
+</script>
+<?php
       // echo "cookie consented to view this content.";
   }
+
+   // Check the status of the Facebook cookie consent , it's a boolean
+   if ($FACEBOOK_track_consent === false || $FACEBOOK_track_consent === 0) {
+    
+    // echo "This content requires cookies to be set.";
+    
+} else { 
+    ?>
+<!-- Meta Pixel Code -->
+<script>
+! function(f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function() {
+        n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+}(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '245524883760394');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=245524883760394&ev=PageView&noscript=1" /></noscript>
+<!-- End Meta Pixel Code -->
+<?php
+    // echo "cookie consented to view this content.";
+}
 }
 ?>
